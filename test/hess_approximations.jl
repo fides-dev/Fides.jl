4e51d11fe0ff8f79f2559825ee79d15fe4803617
sebapersson <sebastian.persson1@gmail.com>
2025-03-19 18:05:51 +0000

    Add Hessian approximations tests

---
+++ test/hess_approximations.jl
@@ -0,0 +1,34 @@
+using Fides, ForwardDiff, Test
+
+function test_hess_approximation(prob, hess_approximation; tol_fmin= 1e-8, tol_xmin = 1e-4)
+    sol = solve(prob, hess_approximation; options = FidesOptions(; maxiter=100000))
+    @test sol.fmin ≈ 0.0 atol=tol_fmin
+    @test all(.≈(sol.xmin, [1.0, 1.0], atol=tol_xmin))
+    return nothing
+end
+
+rosenbrock(u) = (1.0 - u[1])^2 + 100.0 * (u[2] - u[1]^2)^2
+rosenbrock_grad! = (g, x) -> begin
+    ForwardDiff.gradient!(g, rosenbrock, x)
+end
+
+fides_prob = FidesProblem(rosenbrock, rosenbrock_grad!, [2.0, 2.0]; lb = [-10.0, -10.0], ub = [10.0, 10.0])
+
+@testset "Hessian approximations" begin
+    # Good approximation methods, should converge without problems
+    test_hess_approximation(fides_prob, Fides.BFGS())
+    test_hess_approximation(fides_prob, Fides.SR1())
+    test_hess_approximation(fides_prob, Fides.Broyden(0.5))
+    # Worse approximation methods, should converge somewhat
+    test_hess_approximation(fides_prob, Fides.DFP(); tol_fmin = 1e-3, tol_xmin = 1e-1)
+    test_hess_approximation(fides_prob, Fides.BB(); tol_fmin = 1e-3, tol_xmin = 1e-1)
+    test_hess_approximation(fides_prob, Fides.BG(); tol_fmin = 1e-3, tol_xmin = 1e-1)
+    # Try to provide a custom initialization Hessian
+    init_hess = [0.1 0.0; 0.0 0.1]
+    test_hess_approximation(fides_prob, Fides.BFGS(init_hess = init_hess))
+    test_hess_approximation(fides_prob, Fides.SR1(init_hess = init_hess))
+    test_hess_approximation(fides_prob, Fides.Broyden(0.5; init_hess = init_hess))
+    # Other Hessian options
+    test_hess_approximation(fides_prob, Fides.BFGS(enforce_curv_cond = false))
+    test_hess_approximation(fides_prob, Fides.Broyden(0.5; enforce_curv_cond = false))
+end
